f3dff7f13c054cc91a9f4e335b1aaa9bc39021b6
Cole Mengelberg <shougo.yamashiro@gmail.com>
2022-09-01 19:42:38 -0700

    Implemented S3R3
    - Update the accumulated snow amount based on the amount of snow added or lost  during the observation period
    - Given a date in the future, determine the amount of accumulated snow on that date

--- generators.py
+++ generators.py
@@ -32,6 +32,9 @@ class WeatherGen:
 
     Methods
     -------
+    get_target_date()
+        Gets the users desired date for the forcast
+
     timestamp_gen()
         Adds five minutes to the current time and returns a datetime type
 
@@ -61,17 +64,27 @@ class WeatherGen:
     def __init__(self):
         self.WeatherObservation = namedtuple('WeatherObservation',
                                              'timestamp temperature dewpoint humidity barometric_pressure')
+        self.get_target_date()
         self.temp = self.temperature_gen()
         self.weather = self.WeatherObservation(datetime.datetime(2022, 1, 1, 0, 0, 0), self.temp,
                                                self.dewpoint_gen(), self.humidity_gen(), self.barometricp_gen())
+        while self.weather.timestamp != self.target_date:
+            self.__next__()
+
+        print(f"""\n\nDate: {self.weather.timestamp.strftime('%Y, %B, %d %I:%M%p')}
+            Temperature: {self.weather.temperature}C
+            Dewpoint: {self.weather.dewpoint}
+            Humidity: {self.weather.humidity}%
+            Barometric pressure: {self.weather.barometric_pressure}
+            Total snow amount: {round(WeatherGen.total_snow, 3)} meters""")
 
     def __next__(self):
         """
         Checks if precipitation is possible and if it is snow then it adds it to total_snow or if the temperature
-        is higher than 0C or if it rains subtract from total_snow. Then it prints the current weather then
-        generates the next 5 minutes weather and saves it
+        is higher than 0C or if it rains subtract from total_snow. Then it generates the next 5 minutes weather and saves it.
         """
-        precipitation_gen = PrecipitationGen(self.weather.humidity, self.weather.barometric_pressure, self.weather.temperature)
+        precipitation_gen = PrecipitationGen(self.weather.humidity, self.weather.barometric_pressure,
+                                             self.weather.temperature)
         if precipitation_gen.snow_or_rain() == "Snow":
             self.add_snow(precipitation_gen.precipitation_amount())
         if precipitation_gen.snow_or_rain() == "Rain":
@@ -80,15 +93,17 @@ class WeatherGen:
             self.snow_melt_temp()
         if WeatherGen.total_snow < 0:
             WeatherGen.total_snow = 0
-        print(f"""Date: {self.weather.timestamp.strftime('%Y, %B, %A %I:%M%p')}
-    Temperature: {self.weather.temperature}C
-    Dewpoint: {self.weather.dewpoint}
-    Humidity: {self.weather.humidity}%
-    Barometric pressure: {self.weather.barometric_pressure}
-    Total snow amount: {round(WeatherGen.total_snow, 3)} meters""", end='\n\n\n\n')
         self.temp = self.temperature_gen()
         self.weather = self.WeatherObservation(self.timestamp_gen(), self.temp, self.dewpoint_gen(),
                                                self.humidity_gen(), self.barometricp_gen())
+
+    def get_target_date(self):
+        self.year = int(input("Enter a target year (YYYY): "))
+        self.month = int(input("Enter a target month (MM): "))
+        self.day = int(input("Enter a target day (DD): "))
+        self.hour = int(input("Enter a target hour (HH): "))
+        self.min = int(input("Enter a target minute (MM): "))
+        self.target_date = datetime.datetime(self.year, self.month, self.day, self.hour, self.min, 0)
 
     def timestamp_gen(self):
         """
@@ -141,7 +156,6 @@ class WeatherGen:
             return 0.15
         else:
             return 0
-
 
 
 class PrecipitationGen:
