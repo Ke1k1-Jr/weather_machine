79a4d0181142657325cd8fbe4c86792fc8a13179
Cole Mengelberg <shougo.yamashiro@gmail.com>
2022-09-01 19:36:49 -0700

    Implemented S3R2
    - S3R2: Given a WeatherObservation in which rain will occur, calculate the amount of accumulated snow that will melt during the observation period.  The rate of loss should be the same as without rain but multiplied by 1.25 (e.g. 1mm * 1.25 = 1.25mm)

--- generators.py
+++ generators.py
@@ -51,6 +51,9 @@ class WeatherGen:
 
     add_snow(precipitation_amount)
         Returns the amount of water that would turn into snow and add it to the total_snow
+
+    snow_melt_temp():
+        If the temperature is above 0C subtract a certain number from total_snow
     """
 
     total_snow = 0
@@ -64,18 +67,25 @@ class WeatherGen:
 
     def __next__(self):
         """
-        Checks if precipitation is possible and if it is snow then it adds it to total_snow then prints the current
-        weather then generates the next 5 minutes weather and saves it
+        Checks if precipitation is possible and if it is snow then it adds it to total_snow or if the temperature
+        is higher than 0C or if it rains subtract from total_snow. Then it prints the current weather then
+        generates the next 5 minutes weather and saves it
         """
         precipitation_gen = PrecipitationGen(self.weather.humidity, self.weather.barometric_pressure, self.weather.temperature)
         if precipitation_gen.snow_or_rain() == "Snow":
             self.add_snow(precipitation_gen.precipitation_amount())
+        if precipitation_gen.snow_or_rain() == "Rain":
+            WeatherGen.total_snow -= precipitation_gen.precipitation_amount()
+        if self.temp > 0:
+            self.snow_melt_temp()
+        if WeatherGen.total_snow < 0:
+            WeatherGen.total_snow = 0
         print(f"""Date: {self.weather.timestamp.strftime('%Y, %B, %A %I:%M%p')}
     Temperature: {self.weather.temperature}C
     Dewpoint: {self.weather.dewpoint}
     Humidity: {self.weather.humidity}%
     Barometric pressure: {self.weather.barometric_pressure}
-    Total snow amount: {round(WeatherGen.total_snow, 3)}meters""", end='\n\n\n\n')
+    Total snow amount: {round(WeatherGen.total_snow, 3)} meters""", end='\n\n\n\n')
         self.temp = self.temperature_gen()
         self.weather = self.WeatherObservation(self.timestamp_gen(), self.temp, self.dewpoint_gen(),
                                                self.humidity_gen(), self.barometricp_gen())
@@ -116,11 +126,23 @@ class WeatherGen:
         Depending on the temperature how much water turns into snow is multiplied by a certain number
         """
         if -1 >= self.temp >= -3:
-            WeatherGen.total_snow += precipitation_amount * 0.01
+            WeatherGen.total_snow += precipitation_amount * .05
         if -4 >= self.temp >= -6:
-            WeatherGen.total_snow += precipitation_amount * 0.015
+            WeatherGen.total_snow += precipitation_amount * .2
         else:
-            WeatherGen.total_snow += precipitation_amount * 0.02
+            WeatherGen.total_snow += precipitation_amount * .5
+
+    def snow_melt_temp(self):
+        if self.weather.temperature == 1:
+            return 0.05
+        elif self.weather.temperature == 2:
+            return 0.1
+        elif self.weather.temperature == 3:
+            return 0.15
+        else:
+            return 0
+
+
 
 class PrecipitationGen:
     """
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import generators
 
 test = generators.WeatherGen()
-for i in range(100):
-    next(test)
 
-
-# help(generators)
 
